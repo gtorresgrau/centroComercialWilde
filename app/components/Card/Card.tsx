b55c10be404273a993716f4e665e767452bbfd34
Ezequiel Olier <93696524+ezeoli@users.noreply.github.com>
2023-10-20 23:39:17 -0300

    css

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -44,9 +44,11 @@ const Card: React.FC<CardProps> = ({ product }) => {
           className="object-cover w-full h-full"
         />
       </div>
-      <div className="p-4">
+      <div className="p-4 ">
+        <div className=" border border-black rounded-lg -mt-8 bg-white">
         <h2 className="text-xl font-semibold text-offblack text-center mb-2">{local}</h2>
         <h3 className="text-sm text-gray-700 text-center mb-4">{ubicacion}</h3>
+        </div>
         <div className="flex justify-center">
           <button
             className="bg-lightgrey text-white font-medium py-2 px-6 rounded-full hover:bg-[#701a75] transition-transform transform hover:scale-110"
