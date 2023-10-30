d5e9571741bd18232a4005c33fcf79e4979fe12e
Gonzalo <gonzalotorresgrau@gmail.com>
2023-10-30 13:11:20 -0300

    images

--- app/components/Locales/Modal.tsx
+++ app/components/Locales/Modal.tsx
@@ -55,7 +55,7 @@ export default function Modal(props:any) {
               >
                 <Dialog.Panel className="flex flex-col justify-center items-center w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                 {imageLoaded && <Loading />}
-                <Image src={fotoLocal} alt={local} className='rounded-xl shadow-lg' style={{ maxHeight: '350px', width:'370px'}} width={500} height={350} onLoad={handleImageLoad}/> 
+                <Image src={fotoLocal} alt={local} className='rounded-xl shadow-lg object-cover' style={{ maxHeight: '370px', maxWidth:'370px'}} width={370} height={370} onLoad={handleImageLoad}/> 
                 
                   <div className="mt-2">
                       {fraseUsuario
