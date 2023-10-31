4808407a68b544130b39a682489bda5ede2251f1
matias <matiasrozas.lp@gmail.com>
2023-10-31 00:30:36 -0300

    seccion home vista completa

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
    
     return (
         <main className='banner-image'>
-            <section className="relative pb-6">
+            <section className="relative pb-6" style={{height:'100vh'}}>
                 <article className="mx-auto max-w-5xl pt-8 sm:pt-12 sm:pb-12">
                     <div className="text-center">
                         <h1 className="text-3xl sm:text-5xl font-bold tracking-tight text-gray-900 my-4">{userinfo.banner.title}</h1>
