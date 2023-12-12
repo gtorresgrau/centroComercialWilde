618375c3098aced7d55002b93a9497e5c29ddb5a
Gonzalo <gonzalotorresgrau@gmail.com>
2023-12-12 13:43:50 -0300

    categorias

--- server/utils/rubros.tsx
+++ server/utils/rubros.tsx
@@ -12,15 +12,15 @@ rubrosk.sort((a, b) => a.value.localeCompare(b.value));
 
 
 
-export const rubrosGral = [
-  {rubro:'Administracion',locales:['Administracion']},
-  {rubro:'Electronica',locales:['Computacion','Venta Celulares']},
-  {rubro:'Comida',locales:['Almacen','Carniceria','Fiambreria','Verduleria']},
-  {rubro:'Comidas preparadas',locales:['Pasteleria','Pizzeria','Heladeria']},
-  {rubro:'Hogar y Oficina',locales:['Bazar','Descartables','Articulos de Limpieza','Ropa Blanca','Tienda Holistica','Libreria',]},
-  {rubro:'Servicio',locales:['Consultorios','Estudio Contable', 'Inmobiliaria','Optica','Ortopedia','Seguros']},
-  {rubro:'Entretenimiento',locales:['Loteria','Jugueteria']},
-  {rubro:'Cuidado Personal',locales:['Peluqieria','Perfumería y Art. de Limpieza.']},
-  {rubro:'Indumentaria',locales:['Merceria','Ropa Americana','Ropa Bebe','Ropa Dama','Ropa Hombre','Ropa Infantil']},
-  {rubro:'Pet Shop',locales:['Pet Shop']}
+export const categorias = [
+  {categoria:'Administracion',locales:['Administracion']},
+  {categoria:'Electronica',locales:['Computacion','Venta Celulares']},
+  {categoria:'Comida',locales:['Almacen','Carniceria','Fiambreria','Verduleria']},
+  {categoria:'Comidas preparadas',locales:['Pasteleria','Pizzeria','Heladeria']},
+  {categoria:'Hogar y Oficina',locales:['Bazar','Descartables','Articulos de Limpieza','Ropa Blanca','Tienda Holistica','Libreria',]},
+  {categoria:'Servicio',locales:['Consultorios','Estudio Contable', 'Inmobiliaria','Optica','Ortopedia','Seguros']},
+  {categoria:'Entretenimiento',locales:['Loteria','Jugueteria']},
+  {categoria:'Cuidado Personal',locales:['Peluqieria','Perfumería y Art. de Limpieza.']},
+  {categoria:'Indumentaria',locales:['Merceria','Ropa Americana','Ropa Bebe','Ropa Dama','Ropa Hombre','Ropa Infantil']},
+  {categoria:'Pet Shop',locales:['Pet Shop']}
 ]
--- src/components/Locales/Dropdownone.tsx
+++ src/components/Locales/Dropdownone.tsx
@@ -1,12 +1,12 @@
 import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
-import {rubrosk, rubrosGral} from '../../../server/utils/rubros'
+import {rubrosk, categorias} from '../../../server/utils/rubros'
 import { userinfo } from '../../app/Constants/userinfo'
 
 const Dropdown = (props:any) => {
   const [selected, setSelected] = useState(rubrosk[0]);
-  const [gral,setGral] = useState(rubrosGral[0])
+  const [gral,setGral] = useState(categorias[0])
 
   const handleRubro = ()=>{
     props.selectRubro(selected.value)
@@ -17,7 +17,7 @@ const Dropdown = (props:any) => {
   }
 
   console.log('rubrosOk:',rubrosk)
-  console.log('rubrosGral:',rubrosGral)
+  console.log('categorias:',categorias)
 
   
   return (
@@ -25,6 +25,7 @@ const Dropdown = (props:any) => {
       <div className='w-full col-span-2' >
         <Listbox value={selected} onChange={(newValue) => {setSelected(newValue)}}>
           <h2 className='text-lg text-lightgrey'>¿Que estas buscando?</h2>
+          {/* First select */}
           <div className="relative mt-1">
             <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white text-xl py-2 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
               <p className="block truncate text-xl font-semibold ">{selected.value}</p>
@@ -57,6 +58,40 @@ const Dropdown = (props:any) => {
               </Listbox.Options>
             </Transition>
           </div>
+
+          {/* Second select */}
+          {/* <div className="relative mt-1">
+            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white text-xl py-2 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+              <p className="block truncate text-xl font-semibold ">{selected.value}</p>
+              <p className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+              </p>
+            </Listbox.Button>
+            <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+              <Listbox.Options  className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {rubrosk.map((rubro, rurboIdx) => (
+                  <Listbox.Option
+                  key={rurboIdx}
+                  className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900' }`}
+                  value={rubro}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <p className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                          {rubro.value}
+                        </p>
+                        {selected ? (
+                          <p className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </p>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div> */}
         </Listbox>
       </div>
       <div  className=' col-span-1 mt-2 lg:col-span-1 '  >
