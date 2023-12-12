34d669e4642c59d9fd4a1e7ecb4926e24e6a9207
Gonzalo <gonzalotorresgrau@gmail.com>
2023-12-12 11:51:56 -0300

    carrusel

--- src/components/Carrusel/dataCarrusel.js
+++ src/components/Carrusel/dataCarrusel.js
@@ -8,5 +8,12 @@ export const dataCarrusel = [
     { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1697431781/BGerase/Fernando_Mack_dq0e8v.png' },
     { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1697431780/BGerase/Claudia_Claudia_aonf6k.png' },
     { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1697431778/BGerase/Ana_Maria_Molla_cmucqw.png' },
-    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1697431775/BGerase/Administraci%C3%B3n_l8mnij.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392286/BGerase/suQuinta_wiul4g.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392286/BGerase/stellaMaris_k2d6ac.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392285/BGerase/laCaba%C3%B1a_kgznkc.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392286/BGerase/seguros_rwwce5.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392286/BGerase/optica_pnmhsn.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392286/BGerase/serGrande_y0kwdk.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392285/BGerase/artico_tnxht0.png' },
+    { imgSrc: 'https://res.cloudinary.com/drp1ws8hy/image/upload/v1702392285/BGerase/Marino_qg3dbp.png' },
 ];
