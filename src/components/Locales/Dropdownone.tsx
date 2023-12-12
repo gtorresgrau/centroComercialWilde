9772df4e58a6a4f07d350b581b0cb7722b906910
matias <matiasrozas.lp@gmail.com>
2023-12-12 14:51:14 -0300

    filtros

--- src/components/Locales/Dropdownone.tsx
+++ src/components/Locales/Dropdownone.tsx
@@ -6,10 +6,9 @@ import { userinfo } from '../../app/Constants/userinfo'
 
 const Dropdown = (props:any) => {
   const [selected, setSelected] = useState(rubrosk[0]);
-  const [gral,setGral] = useState(categorias[0])
-
+  const [category,setCategory] = useState(categorias[0])
   const handleRubro = ()=>{
-    props.selectRubro(selected.value)
+    props.selectRubro(category.categoria)
   }
   const handleAll = ()=>{
     props.selectRubro('All')
@@ -23,28 +22,28 @@ const Dropdown = (props:any) => {
   return (
     <section className="grid grid-cols-2 items-center lg:grid-cols-4 grid-rows-2 lg:grid-rows-1 gap-3 p-4 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] rounded"> 
       <div className='w-full col-span-2' >
-        <Listbox value={selected} onChange={(newValue) => {setSelected(newValue)}}>
-          <h2 className='text-lg text-lightgrey'>¿Que estas buscando?</h2>
+      <Listbox value={category} onChange={(newValue) => {setCategory(newValue)}}>
+        <h2 className='text-lg text-lightgrey'>¿Que estas buscando?</h2>
           {/* First select */}
           <div className="relative mt-1">
             <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white text-xl py-2 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-              <p className="block truncate text-xl font-semibold ">{selected.value}</p>
-              <p className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <p className="block truncate text-xl font-semibold ">{category.categoria}</p>
+            <p className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
               </p>
             </Listbox.Button>
             <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
               <Listbox.Options  className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {rubrosk.map((rubro, rurboIdx) => (
-                  <Listbox.Option
-                  key={rurboIdx}
+              {categorias.map((name, key) => (
+                <Listbox.Option
+                  key={key}
                   className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900' }`}
-                  value={rubro}
+                  value={name}
                   >
                     {({ selected }) => (
                       <>
                         <p className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                          {rubro.value}
+                          {name.categoria}
                         </p>
                         {selected ? (
                           <p className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
@@ -58,41 +57,16 @@ const Dropdown = (props:any) => {
               </Listbox.Options>
             </Transition>
           </div>
-
-          {/* Second select */}
-          {/* <div className="relative mt-1">
-            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white text-xl py-2 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-              <p className="block truncate text-xl font-semibold ">{selected.value}</p>
-              <p className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                <ChevronDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
-              </p>
-            </Listbox.Button>
-            <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-              <Listbox.Options  className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {rubrosk.map((rubro, rurboIdx) => (
-                  <Listbox.Option
-                  key={rurboIdx}
-                  className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900' }`}
-                  value={rubro}
-                  >
-                    {({ selected }) => (
-                      <>
-                        <p className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                          {rubro.value}
-                        </p>
-                        {selected ? (
-                          <p className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                          </p>
-                        ) : null}
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div> */}
         </Listbox>
+        
+      {/* si categorias.locales que es un array es mayor a 1 renderiza checkbox, sino no.
+          si renderiza checkbox,
+          si no hay check renderiza todos los rubros
+           */}
+
+
+
+
       </div>
       <div  className=' col-span-1 mt-2 lg:col-span-1 '  >
         <button onClick={handleRubro} className="bg-purple w-full hover:bg-pruple text-white font-bold py-4 px-3 rounded">{userinfo.banner.button}</button>
