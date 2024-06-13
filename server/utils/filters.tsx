496bf78ac4767a5ea1c2f45808fba0df9550f7e1
Gonzalo <gonzalotorresgrau@gmail.com>
2024-06-13 10:02:52 -0300

    fav

--- server/utils/filters.tsx
+++ server/utils/filters.tsx
@@ -1,6 +1,6 @@
 import locales from '@/src/app/Constants/data.json'
 
-console.log('locales en filters:',locales)
+//console.log('locales en filters:',locales)
 
 export const filterCat =(data:any)=>{
    const filtro = locales.filter((local) => local.categoria === data);
