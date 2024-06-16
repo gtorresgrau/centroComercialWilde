5d964ac3b090833c1d78501493a7340b538e5b24
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2024-06-16 02:02:29 -0300

    metadata layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import '../styles/globals.css'; // Import global styles
+import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer/Footer';
 import ButtonWsp from '../components/Socials/ButtonWsp';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import AOS styles within the component
+import "aos/dist/aos.css";
+
+// Define metadata with viewport and theme-color
+export const metadata = {
+  manifest: "/manifest.json",
+  title: 'Centro Comercial Wilde',
+  description: 'Paseo de Compras Multimarca',
+  viewport: 'width=device-width, initial-scale=1',
+  themeColor: '#451F49', // Adjust as needed
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  AOS.init(); // Initialize AOS within the component
-
   return (
     <html lang="es">
+      <head>
+        {/* Inject viewport and theme-color from metadata */}
+        <meta name="viewport" content={metadata.viewport} />
+        <meta name="theme-color" content={metadata.themeColor} />
+
+        {/* ... other head elements (e.g., title, description) */}
+      </head>
       <body>
         <Navbar />
         {children}
