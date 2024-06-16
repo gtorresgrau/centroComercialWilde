0801eea78fa2da910d01633101895c8f0c159262
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2024-06-16 01:20:14 -0300

    const withPWA = require('next-pwa')({   dest: "public", });

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const withPWA = require('next-pwa')({
   dest: "public", 
-  cacheOnFrontEndNav: true,
-  reloadOnOnline: true,
-  disable: false,
-  disableDevLogs: true
 });
 
 module.exports = withPWA({
