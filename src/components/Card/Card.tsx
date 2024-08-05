f8cc29378f4402c24e29a5ef5e4c53c1a413f52a
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2024-08-05 19:52:55 -0300

    bg-white

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -39,11 +39,12 @@ const Card: React.FC<CardProps> = ({product }) => {
       key={n_local}
       className="group relative shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] bg-white rounded-lg transition-transform transform hover:scale-105 hover:shadow-lg"
     >
-      <div className="w-full p-2 h-80 overflow-hidden bg-gray-200">
+      <div className="w-full p-2 h-80 overflow-hidden bg-white">
         <img
           src={logoLocal}
           width={500}
           height={500}
+          aria-label={`Product Image: ${ubicacion}`}
           alt={`Product Image: ${ubicacion}`}
           className="object-cover w-full h-full rounded-md "
         />
