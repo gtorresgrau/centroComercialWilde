faf1b75e1d96c026c76ffa0a8a6c00b00611cd1f
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-06 14:15:52 -0300

    ../../../server/utils/mongodb.

--- pages/api/sorteos/sorteos.js
+++ pages/api/sorteos/sorteos.js
@@ -1,5 +1,5 @@
 import { connectDB } from '../../../server/utils/mongodb';
-import Sorteo from '../../src/models/sorteos';
+import Sorteo from '../../../src/models/sorteos';
 
 // Controlador para el método POST
 async function handlePost(req, res) {
