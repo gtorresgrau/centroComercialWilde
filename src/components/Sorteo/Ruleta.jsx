3eba77ce0a50337375ed7bcc910fcd8d92701adb
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-09 14:58:55 -0300

    estructura sorteo

--- src/components/Sorteo/Ruleta.jsx
+++ src/components/Sorteo/Ruleta.jsx
@@ -1,42 +1,31 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import confetti from 'canvas-confetti';
-
 
 const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const torresR = {
-  torre: [1, 3, 4, 14, 17, 18, 21, 22, 23, 24, 25, 28, 29, 32, 33, 35, 36, 38, 39, 44],
-  pisos: 11,
-  deptos: ['A', 'B', 'C', 'D'],
-};
-const torresM = {
-  torre: [5, 6, 8, 9, 11, 12, 13, 15, 19, 20, 26, 27, 30, 31, 40, 42, 43, 45, 46, 47, 48],
-  pisos: 10,
-  deptos: ['A', 'B', 'C', 'D'],
-};
-const torresV = {
-  torre: [2, 7, 10, 16, 34, 37, 41],
-  pisos: 6,
-  deptos: ['A', 'B', 'C', 'D'],
+const torres = {
+  R: { torre: [1, 3, 4, 14, 17, 18, 21, 22, 23, 24, 25, 28, 29, 32, 33, 35, 36, 38, 39, 44], pisos: 11, deptos: ['A', 'B', 'C', 'D'] },
+  M: { torre: [5, 6, 8, 9, 11, 12, 13, 15, 19, 20, 26, 27, 30, 31, 40, 42, 43, 45, 46, 47, 48], pisos: 10, deptos: ['A', 'B', 'C', 'D'] },
+  V: { torre: [2, 7, 10, 16, 34, 37, 41], pisos: 6, deptos: ['A', 'B', 'C', 'D'] }
 };
 
-const sortearCandidatos = () => {
+const sortResults = () => {
   const resultados = [];
   while (resultados.length < 4) {
     const torre = generateRandomNumber(1, 48);
     let cantPisos;
 
-    if (torresR.torre.includes(torre)) {
-      cantPisos = torresR.pisos;
-    } else if (torresM.torre.includes(torre)) {
-      cantPisos = torresM.pisos;
+    if (torres.R.torre.includes(torre)) {
+      cantPisos = torres.R.pisos;
+    } else if (torres.M.torre.includes(torre)) {
+      cantPisos = torres.M.pisos;
     } else {
-      cantPisos = torresV.pisos;
+      cantPisos = torres.V.pisos;
     }
 
-    const piso = generateRandomNumber(0, cantPisos);
-    const depto = torresR.deptos[generateRandomNumber(0, 3)];
+    const piso = generateRandomNumber(1, cantPisos);
+    const depto = torres.R.deptos[generateRandomNumber(0, 3)];
     const resultado = `Torre ${torre} Piso ${piso} Depto ${depto}`;
 
     if (!resultados.includes(resultado)) {
@@ -47,110 +36,80 @@ const sortearCandidatos = () => {
 };
 
 const Ruleta = () => {
-
-  const [state, setState] = useState({
-    ganadores: [],
-    suplentes: [],
-    loading: false,
-    loadingIndex: 0,
-    randomDisplay: 'Torre 1 Piso 0 Depto A',
-  });
+  const [ganadores, setGanadores] = useState([]);
+  const [suplentes, setSuplentes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingIndex, setLoadingIndex] = useState(0);
+  const [randomDisplay, setRandomDisplay] = useState('Torre 1 Piso 0 Depto A');
 
   const handleCelebrar = () => {
-    // Genera confetti
     confetti({
-      particleCount: 500, // Aumenta la cantidad de confetti
-      spread: 160, // Aumenta el ángulo de dispersión
+      particleCount: 500,
+      spread: 160,
       startVelocity: 90,
       decay: 0.9,
       gravity: 0.1,
-      origin: { y: 1.3 }, // Origen de la lluvia de confetti
+      origin: { y: 1.3 },
     });
   };
-  
 
-  const handleSortear = () => {
-    const resultados = sortearCandidatos();
-    setState((prev) => ({ ...prev, loading: true, ganadores: [], suplentes: [], loadingIndex: 0 }));
-
-    const loadResults = (index) => {
-      if (index < resultados.length) {
-        const randomDuration = 2500;
-
-        const showRandom = setInterval(() => {
-          setState((prev) => ({
-            ...prev,
-            randomDisplay: `Torre ${generateRandomNumber(1, 48)} Piso ${generateRandomNumber(0, 11)} Depto ${['A', 'B', 'C', 'D'][generateRandomNumber(0, 3)]}`,
-          }));
-        }, 150);
-
-        setTimeout(() => {
-          clearInterval(showRandom);
-          const ganador = resultados[index];
-          setState((prev) => {
-            const updatedGanadores = index < 2 ? [...prev.ganadores, ganador] : prev.ganadores;
-            const updatedSuplentes = index >= 2 ? [...prev.suplentes, ganador] : prev.suplentes;
-
-            return {
-              ...prev,
-              ganadores: updatedGanadores,
-              suplentes: updatedSuplentes,
-              randomDisplay: ganador,
-              loadingIndex: index + 1,
-            };
-          });
-
-          // Llama directamente a loadResults(index + 1) sin un retraso
-          loadResults(index + 1);
-        }, randomDuration);
-      } else {
-        setState((prev) => ({ ...prev, loading: false }));
-  // Llama a handleCelebrar cada 200 ms
-  let celebracionCount = 4 // Número de veces que quieres llamar a handleCelebrar
-  const interval = setInterval(() => {
-      handleCelebrar();
-      celebracionCount -= 1;
-
-      if (celebracionCount <= 0) {
-          clearInterval(interval); // Detener el intervalo después de 3 llamadas
-      }
-  }, 350); // Cada 200 milisegundos
-      }
-    };
-
-    loadResults(0);
-  };
-
-  useEffect(() => {
-    let interval;
-    if (state.loading) {
-      interval = setInterval(() => {
-        setState((prev) => ({
-          ...prev,
-          randomDisplay: `Torre ${generateRandomNumber(1, 48)} Piso ${generateRandomNumber(0, 11)} Depto ${['A', 'B', 'C', 'D'][generateRandomNumber(0, 3)]}`,
-        }));
+  const loadResults = (index, resultados) => {
+    if (index < resultados.length) {
+      const randomDuration = 2500;
+      const showRandom = setInterval(() => {
+        setRandomDisplay(`Torre ${generateRandomNumber(1, 48)} Piso ${generateRandomNumber(1, 11)} Depto ${['A', 'B', 'C', 'D'][generateRandomNumber(0, 3)]}`);
       }, 150);
 
-      return () => clearInterval(interval);
+      setTimeout(() => {
+        clearInterval(showRandom);
+        const ganador = resultados[index];
+
+        if (index < 2) setGanadores((prev) => [...prev, ganador]);
+        else setSuplentes((prev) => [...prev, ganador]);
+
+        setRandomDisplay(ganador);
+        setLoadingIndex(index + 1);
+
+        loadResults(index + 1, resultados);
+      }, randomDuration);
+    } else {
+      setLoading(false);
+      let celebracionCount = 4;
+      const interval = setInterval(() => {
+        handleCelebrar();
+        celebracionCount -= 1;
+
+        if (celebracionCount <= 0) {
+          clearInterval(interval);
+        }
+      }, 350);
     }
-  }, [state.loading]);
+  };
+
+  const handleSortear = () => {
+    const resultados = sortResults();
+    setLoading(true);
+    setGanadores([]);
+    setSuplentes([]);
+    setLoadingIndex(0);
+    loadResults(0, resultados);
+  };
 
   const renderResult = (result) => {
-    // console.log(result)
-    const partes = result.split(' ');
+    const [_, torre, __, piso, ___, depto] = result.split(' ');
     return (
       <div className='grid grid-cols-3 items-center min-h-[60px]'>
         <div className='grid grid-cols-2 gap-2 text-center'>
           <p className='font-bold'>Torre</p>
-          <p>{partes[1]}</p>
+          <p>{torre}</p>
         </div>
         <div className='grid grid-cols-2 gap-2'>
           <p className='font-bold'>Piso</p>
-          <p>{partes[3]}</p>
+          <p>{piso}</p>
         </div>
         <div className='grid grid-cols-2 gap-2'>
           <p className='font-bold'>Depto</p>
-          <p>{partes[5]}</p>
+          <p>{depto}</p>
         </div>
       </div>
     );
@@ -158,35 +117,37 @@ const Ruleta = () => {
 
   return (
     <section id='ruleta' className='bg-bgpink px-4 text-center'>
-    
       <h1 className="text-2xl md:text-4xl font-bold tracking-tight text-gray-900 py-4" style={{ background: 'linear-gradient(to right, #9C27B0, #1E1E1E)', WebkitBackgroundClip: 'text', color: 'transparent' }}>
         SORTEO DE EXPENSAS PARA CHW
       </h1>
       <span className='text-center text-xs md:text-base'>
-        El sorteo se realiza con este mismo sistema desde la administración del centro comercial wilde para los departamentos del Complejo Habitacional Wilde.
+        El sorteo se realiza con este mismo sistema desde la administración del Centro Comercial Wilde para los departamentos del Complejo Habitacional Wilde.
+      </span><br/>
+      <span className='text-center text-xs md:text-base'>
+        ¡Ya hay varios ganadores, y vos podés ser el próximo! <strong>¡Anotate fácil y rápido!</strong>
       </span>
       <div className='flex justify-around items-center mt-4'>
         <div className="items-center mx-auto max-w-2xl px-1 pb-32 mb-32 lg:max-w-7xl lg:px-4">
           <div className='mb-4 md:min-h-[100px] md:min-w-[600px] w-full'>
             <div className='flex flex-col md:flex-row items-center text-center'>
-              <div className=''>
-                <button className="min-w-[150px] bg-transparent hover:bg-purple text-purple font-semibold hover:text-white py-3 px-2 border border-lightgrehover:border-transparent rounded m-4 md:my-0" onClick={handleSortear} disabled={state.loading}>
-                  {state.loading && state.loadingIndex < 4 ? 'SORTEANDO...' : 'SORTEAR'}
+              <div>
+                <button className="min-w-[150px] bg-transparent hover:bg-purple text-purple font-semibold hover:text-white py-3 px-2 border border-lightgrehover:border-transparent rounded m-4 md:my-0" onClick={handleSortear} disabled={loading}>
+                  {loading && loadingIndex < 4 ? 'SORTEANDO...' : 'SORTEAR'}
                 </button>
               </div>
               <div className="flex flex-col lg:flex-row gap-4 w-full">
                 <div className='min-h-[178px] w-full xs:min-w-[320px] sm:min-w-[370px] border border-purple rounded'>
-                  <div className=' px-2 py-4'>
+                  <div className='px-2 py-4'>
                     <h2><strong>GANADORES</strong></h2>
-                    {state.ganadores.map((ganador) => renderResult(ganador))}
-                    {state.loading && state.loadingIndex < 2 && renderResult(state.randomDisplay)}
+                    {ganadores.map((ganador) => renderResult(ganador))}
+                    {loading && loadingIndex < 2 && renderResult(randomDisplay)}
                   </div>
                 </div>
                 <div className='min-h-[178px] w-full xs:min-w-[320px] sm:min-w-[370px] border border-purple'>
-                  <div className=' rounded px-2 py-4'>
+                  <div className='rounded px-2 py-4'>
                     <h2><strong>SUPLENTES</strong></h2>
-                    {state.suplentes.map((suplente) => renderResult(suplente))}
-                    {state.loading && state.loadingIndex > 1 && state.loadingIndex < 4 && renderResult(state.randomDisplay)}
+                    {suplentes.map((suplente) => renderResult(suplente))}
+                    {loading && loadingIndex > 1 && loadingIndex < 4 && renderResult(randomDisplay)}
                   </div>
                 </div>
               </div>
--- src/components/Sorteo/Sorteo.jsx
+++ src/components/Sorteo/Sorteo.jsx
@@ -1,35 +1,37 @@
 import React from 'react';
 import ContactoSorteo from './contactoSorteo';
 import Link from 'next/link';
-import  './sorteo.css';
+import './sorteo.css';
+
 const Sorteo = () => {
   return (
     <section id='sorteo' className='bg-bgpink'>
-        <div className="items-center mx-auto max-w-2xl px-4 pt-16 pb-32 sm:pt-32 lg:max-w-7xl lg:px-8">    
-            <div className='grid grid-cols-1justify-around items-center justify-self-center'>
-                <div className='grid grid-cols-1 mb-4 items-center text-center'>
-                    <h2 className="text-4xl md:text-5xl font-bold tracking-tight text-gray-900 my-4 mb-8 text-center lg:text-start " 
-                        style={{ 
-                            background: 'linear-gradient(to right, #9C27B0, #1E1E1E)',
-                            WebkitBackgroundClip: 'text',
-                            color: 'transparent',
-                        }}>SORTEAMOS <strong>2</strong> EXPENSAS del <br/> COMPLEJO HABITACIONAL WILDE
-                    </h2>
-                </div>
-                <div className="grid grid-cols-1 text-center place-items-center md:grid-cols-2">
-                    <div className="">
-                        <span className='text-lg font-medium'><strong>ANOTATE!</strong> este puede ser tu mes</span><br/>
-                        <Link href='/ruleta'>
-                            <div className=' arrow my-6 md:my-1' />
-                            <button className="bg-transparent hover:bg-purple text-purple font-semibold hover:text-white py-3 px-4 border border-lightgrehover:border-transparent rounded"> Mira cómo se hace el sorteo! </button>
-                        </Link>
-                    </div>
-                    <div className="my-4">
-                        <ContactoSorteo />
-                    </div>
-                </div>
+      <div className="items-center mx-auto max-w-2xl px-4 pt-16 pb-32 sm:pt-32 lg:max-w-7xl lg:px-8">
+        <div className='grid grid-cols-1 justify-around items-center justify-self-center text-center'>
+          <div className='grid grid-cols-1 mb-4 items-center'>
+            <h2 className="text-4xl md:text-5xl font-bold tracking-tight text-gray-900 my-4 mb-8"
+              style={{ 
+                background: 'linear-gradient(to right, #9C27B0, #1E1E1E)',
+                WebkitBackgroundClip: 'text',
+                color: 'transparent',
+              }}>SORTEAMOS <strong>2</strong> EXPENSAS del <br /> COMPLEJO HABITACIONAL WILDE
+            </h2>
+            <span className='text-lg font-medium'><strong>¡ANOTATE!</strong> Este puede ser tu mes</span><br />
+          </div>
+          <div className="grid grid-cols-1 place-items-center md:grid-cols-2">
+            <div className="">
+              <Link href='/ruleta'>
+                <button className="bg-transparent hover:bg-purple text-purple font-semibold hover:text-white py-3 px-4 border border-lightgrehover:border-transparent rounded">
+                  ¡Mira cómo se hace el sorteo!
+                </button>
+              </Link>
             </div>
+            <div className="my-4">
+              <ContactoSorteo />
+            </div>
+          </div>
         </div>
+      </div>
     </section>
   );
 }
