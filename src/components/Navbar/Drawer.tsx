39e0846bb97add225b7dc721c24d3bb3dd63e200
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2024-08-12 00:31:04 -0300

    aria-hiden

--- src/components/Navbar/Drawer.tsx
+++ src/components/Navbar/Drawer.tsx
@@ -9,7 +9,7 @@ interface DrawerProps {
 
 const Drawer = ({ children, isOpen, setIsOpen }: DrawerProps) => {
     return (
-        <main className={`fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ${isOpen ? "transition-opacity opacity-100 duration-500 translate-x-0" : "transition-all delay-500 opacity-0 -translate-x-full"}`} aria-hidden={!isOpen}>
+        <main className={`fixed overflow-hidden z-10 bg-gray-900 bg-opacity-25 inset-0 transform ease-in-out ${isOpen ? "transition-opacity opacity-100 duration-500 translate-x-0" : "transition-all delay-500 opacity-0 -translate-x-full"}`} aria-label="abrir menu">
             <section className={`w-340px max-w-lg left-0 absolute bg-white h-full shadow-xl delay-400 duration-500 ease-in-out transform ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 <article className="relative w-340px max-w-lg pb-10 flex flex-col space-y-6 h-full">
                     <header className="p-4 flex items-center justify-between">
