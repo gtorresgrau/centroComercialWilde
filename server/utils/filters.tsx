6a10cb1a2c5861e11ac4f68e402f1f468d42c9c5
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2024-08-23 00:53:43 -0300

    filtros

--- server/utils/filters.tsx
+++ server/utils/filters.tsx
@@ -1,13 +1,18 @@
-import locales from '@/src/app/Constants/data.json'
-
-//console.log('locales en filters:',locales)
-
-export const filterCat =(data:any)=>{
-   const filtro = locales.filter((local) => local.categoria === data);
-   return filtro
-}
-
-export const filterLocal =(data:any)=>{
-   const filtro = locales.filter((local) => local.rubro === data.join());
-   return filtro
-}
+export const filterCat = (data: any) => {
+   const cachedLocales = localStorage.getItem('locales');
+   if (!cachedLocales) return []; // Manejar el caso en que no haya locales en localStorage
+ 
+   const locales = JSON.parse(cachedLocales);
+   const filtro = locales.filter((local: any) => local.categoria === data);
+   return filtro;
+ };
+ 
+ export const filterLocal = (data: any) => {
+   const cachedLocales = localStorage.getItem('locales');
+   if (!cachedLocales) return []; // Manejar el caso en que no haya locales en localStorage
+ 
+   const locales = JSON.parse(cachedLocales);
+   const filtro = locales.filter((local: any) => local.rubro === data.join());
+   return filtro;
+ };
+ 
--- src/Utils/fetchLocales.tsx
+++ src/Utils/fetchLocales.tsx
@@ -1,9 +1,18 @@
 export const fetchLocales = async () => {
+    const cachedLocales = localStorage.getItem('locales');
+  
+    if (cachedLocales) {
+      return JSON.parse(cachedLocales);
+    }
+  
     const res = await fetch('/api/locales/locales');
     if (!res.ok) {
       throw new Error('Error al cargar los locales');
     }
+  
     const data = await res.json();
+    localStorage.setItem('locales', JSON.stringify(data.locales));
+  
     return data.locales;
   };
   
