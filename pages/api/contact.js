9074d63c3dbb8fe028a7b1bf74fbeb8010538eb4
Gonzalo <gonzalotorresgrau@gmail.com>
2024-08-27 13:28:58 -0300

    estilos

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,7 +3,7 @@ let nodemailer = require('nodemailer')
 const { SENDER, PASSWORD, TO} = process.env;
 
 export default function (req, res) {
-  
+  //console.log(req.body)
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
@@ -20,14 +20,26 @@ export default function (req, res) {
       from: SENDER,
       to: TO,
       subject: `Sorteo expensas CHW`,
-      html: `<p>La Persona se ha suscripto al sorteo desde el sitio web:</p><p>Nombre: ${req.body.nombre + ' ' +req.body.apellido}</p><p>Email: ${req.body.email}</p><p>Domicilio: Torre ${req.body.torre + ' Piso:' + req.body.piso + ' Depto:' + req.body.depto}</p><p>Terminos: ${req.body.aceptar?'Acepto participar':'yo no me anote'}</p>`,
+      html: 
+        `<p>La Persona se ha suscripto al sorteo desde el sitio web:</p>
+        <p>Nombre: ${req.body.nombre + ' ' +req.body.apellido}</p>
+        <p>Email: ${req.body.email}</p>
+        <p>DNI: ${req.body.dni}</p>
+        <p>Domicilio: Torre ${req.body.torre + ' Piso:' + req.body.piso + ' Depto:' + req.body.depto}</p>
+        <p>Terminos: ${req.body.aceptar?'Acepto participar':'yo no me anote'}</p>`,
     }
   }else{
     mailData = {
       from: SENDER,
       to: TO,
-      subject: 'newsletter' in req.body? `Centro Comercial Wilde - Newsletter`: `Message From Centro Comercial Wilde Website`,
-      html: 'newsletter' in req.body? `<p>El email: ${req.body.newsletter} esta interesado en que le envien informacion de los proximos eventos.</p>`:`<p>La Persona ha hecho contacto desde el sitio web:</p><p>Nombre: ${req.body.nombre}</p><p>Email: ${req.body.email}</p><p>mensaje: ${req.body.msj}</p>`,
+      subject: 'newsletter' in req.body? `Centro Comercial Wilde - Newsletter`: `Mensaje desde Centro Comercial Wilde Website`,
+      html: 
+        'newsletter' in req.body
+          ?`<p>El email: ${req.body.newsletter} esta interesado en que le envien informacion de los proximos eventos.</p>`
+          :`<p>La Persona ha hecho contacto desde el sitio web:</p>
+              <p>Nombre: ${req.body.input1}</p>
+              <p>Email: ${req.body.input2}</p>
+              <p>mensaje: ${req.body.input3}</p>`,
     }
   }
 
