f4bcd705b6c778702bc463ea2080f01cb30e606b
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2024-09-01 15:11:12 -0300

    errores

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -9,9 +9,9 @@ const Banner = () => {
     };
 
     return (
-        <main className="banner-image relative">
-            <section className="items-center relative pb-6 lg:h-screen">
-                <article className="mx-auto max-w-5xl pt-8 sm:py-12 md:py-4 xxl:py-12">
+        <section className="banner-image relative">
+            <article className="items-center relative pb-6 lg:h-screen">
+                <div className="mx-auto max-w-5xl pt-8 sm:py-12 md:py-4 xxl:py-12">
                     <div className="text-center">
                         <h1 className="text-3xl sm:text-5xl font-bold tracking-tight sm:my-6 md:my-2 xl:my-6" style={{ 
                             background: 'linear-gradient(to right, #9C27B0, #1E1E1E)',
@@ -20,16 +20,13 @@ const Banner = () => {
                         }}>{userinfo.banner.title}</h1>
                         <h2 className="text-3xl sm:text-4xl sm:font-bold tracking-tight text-gray-900 sm:text-60px md:4px sm:my-6 md:my-2 xl:my-6">{userinfo.banner.subTitle}</h2>
                     </div>
-                </article>
-                <div 
-                    className={`${s.balloon} cursor-pointer z-10`} 
-                    onClick={handleBalloonClick}
-                >
+                </div>
+                <div className={`${s.balloon} cursor-pointer z-10`} onClick={handleBalloonClick}>
                     SORTEO
                 </div>
                 <Carrusel />
-            </section>
-        </main>
+            </article>
+        </section>
     );
 }
 
