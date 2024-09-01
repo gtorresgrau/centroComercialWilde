14fd31fc10b556521bc35ef1d88c3b3c920328f2
matias <matiasrozas.lp@gmail.com>
2024-09-01 11:05:51 -0300

    connectDB() no conecta nuevamente si hay una conexion abierta

--- server/utils/mongodb.js
+++ server/utils/mongodb.js
@@ -1,19 +1,28 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
+// Cargar variables de entorno desde el archivo .env
 dotenv.config();
 
 const url = process.env.MONGO_LOCALES_URL;
 
 if (!url) {
-  throw new Error('Please define the MONGOURL environment variable inside .env.local');
+  throw new Error('Please define the MONGO_LOCALES_URL environment variable inside .env.local');
 }
 
 export async function connectDB() {
+  // Verifica si ya hay una conexión establecida
+   if (mongoose.connection.readyState >= 1) {
+     console.log('Mongoose is already connected to DB');
+     return;
+   }
+
   try {
     await mongoose.connect(url, {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
+      connectTimeoutMS: 10000, // Tiempo de espera antes de fallar la conexión
+      serverSelectionTimeoutMS: 5000, // Tiempo de espera para seleccionar el servidor
     });
     console.log('Mongoose connected to DB');
   } catch (error) {
@@ -21,7 +30,15 @@ export async function connectDB() {
   }
 }
 
-// Listen to mongoose connection events
+// Escuchar eventos de conexión de mongoose
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
 mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected from DB');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error('Mongoose connection error:', error);
 });
