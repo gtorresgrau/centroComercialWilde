72f4ee8449f43292bd0917005e4acf13b67fcf6f
matias <matiasrozas.lp@gmail.com>
2024-09-05 06:59:25 -0300

    estilos confirmacion de baja y tabla

--- src/components/Admin/TablaNewsletter/TablaNewsletter.jsx
+++ src/components/Admin/TablaNewsletter/TablaNewsletter.jsx
@@ -152,14 +152,14 @@ const TablaNewsletter = () => {
                 <button
                   onClick={handleSendEmails}
                   type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                >
+                  className="items-center text-white border bg-primary hover:bg-[#612c67] active:bg-[#9c47a5] font-medium rounded-lg h-10 text-xs xs:text-sm px-5 py-2 text-center "
+                  >
                   Enviar correos
                 </button>
                 <button
                   onClick={closeModal}
                   type="button"
-                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700"
+                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-bgpurple  hover:bg-gray-100 hover:text-bgpurple"
                 >
                   Cancelar
                 </button>
