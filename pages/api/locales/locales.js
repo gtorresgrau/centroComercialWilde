811aca1823eb5d4a9df7d24911892bd872da0d14
matias <matiasrozas.lp@gmail.com>
2024-09-18 07:21:16 -0300

    get locales solo con activo en true o que no exista esa propiedad

--- pages/api/locales/locales.js
+++ pages/api/locales/locales.js
@@ -4,7 +4,14 @@ import Local from "../../../src/models/locales";
 // Controlador para el método GET
 async function handleGet(req, res) {
   try {
-    const locales = await Local.find();
+    // Buscar documentos donde `activo` es `true` o donde `activo` no existe
+    const locales = await Local.find({
+      $or: [
+        { activo: true }, // Documentos donde `activo` es `true`
+        { activo: { $exists: false } } // Documentos donde `activo` no existe
+      ]
+    });
+
     return res.status(200).json({ locales });
   } catch (error) {
     console.error("Error al obtener los locales:", error);
