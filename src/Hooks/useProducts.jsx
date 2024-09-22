603141eca6f1a92d0bad6c67a088275737d4e828
matias <matiasrozas.lp@gmail.com>
2024-09-22 02:05:30 -0300

    fetch sorteo query tipo de usuario

--- src/Hooks/useProducts.jsx
+++ src/Hooks/useProducts.jsx
@@ -1,192 +1,23 @@
 'use client'
 import { useState, useEffect } from 'react';
-import { useSearchParams, useRouter } from 'next/navigation';
-import { usePathname } from "next/navigation";
 import axios from 'axios';
 
 
-const useProducts = () => {
-  const path = usePathname()
-  // const [products, setProducts] = useState([]);
+const useProducts = (chw) => {
   const [userSorteo, setUserSorteo] = useState([]);
-  const [userSorteoCHW, setUserSorteoCHW] = useState([]);
-  const [userSorteoNoCHW, setUserSorteoNoCHW] = useState([]);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  // const fetchProducts = async () => {
-  //   // const params = new URLSearchParams(searchParams.toString());
-  //   const res = await axios.get(`/api/locales/locales`);
-  //   //console.log(res.data.locales)
-  //   const data = await res.data;
-  //   setProducts(data.locales || []);
-  //   // setTotalPages(data.totalPage || 1);
-  //   // setCategories(data.totalCategories || []);
-  //   // setBrands(data.totalBrands || []);
-  //   // setVehiculos(data.totalVehiculos || []);
-  //   // setAllDestacados(data.allproductosDestacados || []);
-  //   // setIsLoading(false);
-  // };
   const fetchSorteos = async () => {
-    // const params = new URLSearchParams(searchParams.toString());
-    const res = await axios.get(`/api/sorteos`);
-    //console.log(res.data.sorteos)
-    const data = await res.data.sorteos;
-    const usersCHW = data.filter(user => user.chw === true);
-    const usersNoCHW = data.filter(user => user.chw === false);
-    setUserSorteo(data || []);
-    setUserSorteoCHW(usersCHW);
-    setUserSorteoNoCHW(usersNoCHW);
-    // setTotalPages(data.totalPage || 1);
-    // setCategories(data.totalCategories || []);
-    // setBrands(data.totalBrands || []);
-    // setVehiculos(data.totalVehiculos || []);
-    // setAllDestacados(data.allproductosDestacados || []);
-    // setIsLoading(false);
+    const res = await axios.get(`/api/sorteos/getChw?chw=${chw}`);
+    const data = await res.data.sorteos
+    setUserSorteo(data || [])
   };
 
   useEffect(() => {
-    // fetchProducts();
     fetchSorteos();
-  }, [searchParams]);
-
+  }, []);
   
-  const closeModal = () => {
-    // setSelectedProduct(null);
-    setIsModalOpen(false);
-    window.history.pushState(null, null, ' ');
-
-  };
-
-  // const handleProductSelect = (product) => {
-  //   setSelectedProduct(product);
-  //   setIsModalOpen(true);
-  //     window.location.hash = 'producto';
-  // };
-
-  useEffect(() => {
-    const handlePopState = () => {
-      if (window.location.hash !== '#update' && isModalOpen) {
-        closeModal();
-      }
-    };
-
-    window.addEventListener('popstate', handlePopState);
-
-    return () => {
-      window.removeEventListener('popstate', handlePopState);
-    };
-  }, [closeModal]);
-  
-  // useEffect(() => {
-  //   const page = searchParams.get("page") || 1;
-  //   setCurrentPage(Number(page));
-  // }, [searchParams]);
-
-  // useEffect(() => {
-  //   const urlSearchParams = new URLSearchParams(window.location.search);
-  //   setSelectedCategories(urlSearchParams.getAll("category"));
-  //   setSelectedBrands(urlSearchParams.getAll("brand"));
-  //   setSelectedVehiculos(urlSearchParams.getAll("vehiculo"));
-  // }, []);
-
-//   const handlePageChange = (event, value) => {
-// //     setCurrentPage(value);
-//     const params = new URLSearchParams(searchParams.toString());
-//     params.set("page", value);
-//     path === '/Admin'
-//     ? router.push(`/Admin?${params.toString()}`)
-//     : router.push(`/?${params.toString()}#productos`);
-// };
-
-// const handleCheckboxChange = (e, key, selectedValues, setSelectedValues) => {
-//     const value = e.target.value;
-//     const isChecked = e.target.checked;
-//     setSelectedValues((prevSelected) => {
-//         const newSelected = isChecked
-//         ? [...prevSelected, value]
-//         : prevSelected.filter((item) => item !== value);
-//         updateSearchParams(key, newSelected);
-//         return newSelected;
-//     })
-//   };
-
-//   const handleClearFilters = () => {
-//     const params = new URLSearchParams(searchParams.toString());
-//     params.delete("search");
-//     params.delete("category");
-//     params.delete("brand");
-//     params.delete("vehiculo");
-//     params.set("page", 1);
-//     setSelectedCategories([]);
-//     setSelectedBrands([]);
-//     setSelectedVehiculos([]);
-//     path === '/Admin'
-//     ? router.push(`/Admin?${params.toString()}`)
-//     : router.push(`/?${params.toString()}#productos`);
-//   };
-
-//   const handleShowAllCategories = () => {
-//     setShowAllCategories(!showAllCategories);
-//   };
-
-//   const handleShowAllBrands = () => {
-//     setShowAllBrands(!showAllBrands);
-//   };
-
-//   const handleShowAllVehiculos = () => {
-//     setShowAllVehiculos(!showAllVehiculos);
-//   };
-
-  const updateSearchParams = (key, values) => {
-    const params = new URLSearchParams(searchParams.toString());
-    params.delete(key);
-    params.set('page', 1)
-
-    if (values.length > 0) {
-      values.forEach((value) => params.append(key, value));
-    }
-    path === '/Admin'
-    ? router.push(`/Admin?${params.toString()}`)
-    : router.push(`/?${params.toString()}#productos`);
-  };
   return {
-    // products,
-    userSorteo,
-    userSorteoNoCHW,
-    userSorteoCHW,
-    // allDestacados,
-    // categories,
-    // brands,
-    // vehiculos,
-    // selectedCategories,
-    // selectedBrands,
-    // selectedVehiculos,
-    // showAllCategories,
-    // showAllBrands,
-    // showAllVehiculos,
-    // selectedProduct,
-    // isModalOpen,
-    // totalPages,
-    // currentPage,
-    // isLoading,
-    // setIsModalOpen,
-    // setSelectedCategories,
-    // setSelectedBrands,
-    // setSelectedVehiculos,
-    // handlePageChange,
-    // handleCheckboxChange,
-    // handleClearFilters,
-    // handleShowAllCategories,
-    // handleShowAllBrands,
-    // handleShowAllVehiculos,
-    // closeModal,
-    // handleProductSelect,
-    // fetchProducts,
-    fetchSorteos,
+    userSorteo
   };
 };
 
